e49dbdf4d23c832e0cfca619b3a62b6c9c95b674
GrimmOutlook <grimmoutlook87@gmail.com>
2018-05-09 22:59:16 -0400

    Created InputWithButton component with associated files.  Added most styling to it.  Made the Home screen a class component.

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import { StatusBar, Text } from 'react-native';
+import React, { Component } from 'react';
+import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container/index';
+import { Logo } from '../components/Logo';
+import { InputWithButton } from '../components/TextInput';
 
-export default () => (
-  <Container>
-    <StatusBar translucent={false} barStyle="light-content" />
-    <Text>Whats up?!</Text>
-  </Container>
-);
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
+
+class Home extends Component {
+  handlePressBaseCurrency = () => {
+    console.log('press base currency');
+  };
+
+  handlePressQuoteCurrency = () => {
+    console.log('press quote currency');
+  };
+
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlePressBaseCurrency}
+        />
+        <InputWithButton
+          buttonText={TEMP_QUOTE_CURRENCY}
+          onPress={this.handlePressQuoteCurrency}
+          editable={false}
+        />
+      </Container>
+    );
+  }
+}
+
+export default Home;
