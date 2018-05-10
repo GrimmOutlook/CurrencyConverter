a602758bde2ca82a2f8016ce1a76c97a4492af46
GrimmOutlook <grimmoutlook87@gmail.com>
2018-05-10 17:43:56 -0400

    Finalized styling for the InputWithButton & Home components.

--- app/components/TextInput/InputWithButton.js
+++ app/components/TextInput/InputWithButton.js
@@ -1,17 +1,32 @@
 import React, { PropTypes } from 'react';
 import { View, Text, TextInput, TouchableHighlight } from 'react-native';
+import color from 'color';
 
 import styles from './styles';
 
-const InputWithButton = ({ onPress, buttonText, editable = true }) => (
-  <View style={styles.container}>
-    <TouchableHighlight style={styles.buttonContainer} onPress={onPress}>
-      <Text style={styles.buttonText}>{buttonText}</Text>
-    </TouchableHighlight>
-    <View style={styles.border} />
-    <TextInput style={styles.input} editable={editable} />
-  </View>
-);
+const InputWithButton = (props) => {
+  const { onPress, buttonText, editable = true } = props;
+  const containerStyles = [styles.container];
+  const underlayColor = color(styles.$buttonBackgroundColorBase).darken(
+    styles.$buttonBackgroundColorModifier,
+  );
+  if (editable === false) {
+    containerStyles.push(styles.containerDisabled);
+  }
+  return (
+    <View style={styles.container}>
+      <TouchableHighlight
+        underlayColor={underlayColor}
+        style={styles.buttonContainer}
+        onPress={onPress}
+      >
+        <Text style={styles.buttonText}>{buttonText}</Text>
+      </TouchableHighlight>
+      <View style={styles.border} />
+      <TextInput style={styles.input} underlineColorAndroid="transparent" {...props} />
+    </View>
+  );
+};
 
 InputWithButton.propTypes = {
   onPress: PropTypes.func,
