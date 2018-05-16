050f706a5c647f92d350697306a696d7b862df32
GrimmOutlook <grimmoutlook87@gmail.com>
2018-05-16 13:33:38 -0400

    Setup currencies reducer & combineReducers fxn.

---
+++ app/reducers/currencies.js
@@ -0,0 +1,28 @@
+import { SWAP_CURRENCY, CHANGE_CURRENCY_AMOUNT } from './currencies';
+
+const initialState = {
+  baseCurrency: 'USD',
+  quoteCurrency: 'GBP',
+  amount: 100,
+  conversions: {},
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SWAP_CURRENCY:
+      return {
+        ...state,
+        amount: action.amount || 0,
+      };
+    case CHANGE_CURRENCY_AMOUNT:
+    default:
+      return {
+        ...state,
+        baseCurrency: state.quoteCurrency,
+        quoteCurrency: state.baseCurrency,
+      };
+      return state;
+  }
+};
+
+export default reducer;
